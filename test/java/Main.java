0a237231e4eed3d60f2a1b0619191d8ea7acc3a5
1997Gongquan <2673077461@qq.com>
2019-02-13 14:35:28 +0800

    测试文件

---
+++ test/java/Main.java
@@ -0,0 +1,11 @@
+import 注解.MyAnnotation01;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+    @MyAnnotation01(studentName = "龚羽",age=18)
+    void test(){
+        int test;
+    }
+}
